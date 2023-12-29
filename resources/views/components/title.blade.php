397142feef14bf2a7cd4c34e980b33949ed9a155
Adriano <adrianosenny@gmail.com>
2023-12-29 12:54:08 -0300

    Feature: Criando componente titulo

---
+++ resources/views/components/title.blade.php
@@ -0,0 +1,6 @@
+<div class="w-full pt-8 pb-20">
+    <h2 class="relative flex items-center justify-center font-markazi">
+        <span class="absolute z-10 text-[50px] opacity-60 font-krona left-[50.5%] -translate-x-2/4 text-orange-500">{{$japanese}}</span>
+        <span class="uppercase text-zinc-800 text-9xl">{{$title}}</span>
+    </h2>
+</div>
